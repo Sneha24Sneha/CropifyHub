14202c11bb396ab5d89eff0e80a4c43d4e3480b5
pavan <pavan4bit@gmail.com>
2025-11-07 16:10:34 +0530

    Lazy loading updated and bootstrap css link updated

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import Crop from "./pages/crop";
-import Convert from "./pages/convert";
-import Resize from "./pages/resize";
-import Thumbnail from "./pages/thumbnail";
+
+const LandingPage = lazy(() => import("./pages/LandingPage"));
+const Crop = lazy(() => import("./pages/Crop"));
+const Convert = lazy(() => import("./pages/Convert"));
+const Resize = lazy(() => import("./pages/Resize"));
+const Thumbnail = lazy(() => import("./pages/Thumbnail"));
 
 function App() {
-  return (
-   <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/crop" element={<Crop />} />
-        <Route path="/convert" element={<Convert />} />
-        <Route path="/resize" element={<Resize />} />
-        <Route path="/thumbnail" element={<Thumbnail />} />
-      </Routes>
-    </Router>
+  const basenameUrl = import.meta.env.VITE_PUBLIC_URL || "/";
 
+  return (
+    <Router basename={basenameUrl}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/crop" element={<Crop />} />
+          <Route path="/convert" element={<Convert />} />
+          <Route path="/resize" element={<Resize />} />
+          <Route path="/thumbnail" element={<Thumbnail />} />
+          <Route path="*" element={<div>404 - Page Not Found</div>} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
-
 export default App;
